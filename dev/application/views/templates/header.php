7dd713edb664fc574be477449e101911bfe342e5
Scott Lyon <scottdavidlyon@gmail.com>
2013-03-24 16:19:24 -0400

    Hard-coded title.

--- dev/application/views/templates/header.php
+++ dev/application/views/templates/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title><?php $title ?></title>
+	<title><?php // $title ?>VoteForMyTeam.com</title>
 	<link rel="stylesheet" href="/assets/css/style.css" type="text/css" />
 	<link href="/assets/css/SpryTabbedPanels.css" rel="stylesheet" type="text/css">
 	<script src="/assets/js/SpryTabbedPanels.js" type="text/javascript"></script>
